22ddd09247591ffffb0658ef38d346b6e524c2d1
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-01-14 22:02:27 +0530

    Port to JS

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-$(".fa-plus").on("click", function() {
-    $("input").fadeToggle(150);
-});
-
-$("ul").on("click", "li", function() {
-    $(this).toggleClass("done");
-});
-
-$("ul").on("click", "span", function(event) {
-    $(this).parent().fadeOut(200, function() {
-        $(this).remove();
-    });
-    event.stopPropagation();
-});
-
-$("input[type='text']").on("keypress", function() {
-    if (event.key === "Enter") {
-        $("ul").append("<li><span><i class='fas fa-trash'></i></span>" + $(this).val() + "</li>");
-        $(this).val("");
+var notes = [];
+var input = document.getElementById("new_note_input");
+var notes_list = document.getElementById("notes_list");
+input.onkeypress = function (event) {
+    if (event.key == "Enter") {
+        var val = input.value;
+        if (val != "") {
+            input.value = "";
+            notes.push(val);
+            populate_list();
+        }
     }
-});
+}
+
+function populate_list() {
+    notes_list.innerHTML = "";
+    for (var i = 0; i < notes.length; i++) {
+        notes_list.innerHTML += "<li><span></span>" + notes[i] + "</li>";
+    }
+}
+
+notes_list.onclick = function (event) {
+    if (event.target.tagName == "SPAN") {
+        var to_be_deleted = event.target.parentElement.innerText
+        notes.splice(notes.indexOf(to_be_deleted),1)
+        populate_list();
+    } else {
+        var note_li = event.target;
+        if (note_li.style.textDecoration == "line-through") {
+            note_li.style.textDecoration = "";
+            note_li.style.color = "#666";
+        } else {
+            note_li.style.textDecoration = "line-through";
+            note_li.style.color = "gray";
+        }
+    }
+}
